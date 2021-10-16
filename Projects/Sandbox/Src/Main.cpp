17d812aac897b36616a5adeb8e42453d49a8de6f
Jonathan Åleskog <jonathan9609082000@gmail.com>
2021-10-16 17:25:25 +0200

    Added Config

--- Projects/Sandbox/Src/Main.cpp
+++ Projects/Sandbox/Src/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "Core/Config.h"
 #include "Core/Display.h"
 #include "Core/Input.h"
 #include "Core/FrameTimer.h"
@@ -11,13 +12,14 @@ void Tick(const RS::FrameStats& frameStats);
 int main()
 {
     RS::Logger::Init();
+    RS::Config::Get()->Init(RS_CONFIG_FILE_PATH);
 
     RS::DisplayDescription displayDesc = {};
-    displayDesc.Title       = "Sandbox DX12";
-    displayDesc.Width       = 1920;
-    displayDesc.Height      = 1080;
-    displayDesc.Fullscreen  = false;
-    displayDesc.VSync       = true;
+    displayDesc.Title       = RS::Config::Get()->Fetch<std::string>("Display/Title", "Sandbox DX12");
+    displayDesc.Width       = RS::Config::Get()->Fetch<uint32>("Display/DefaultWidth", 1920);
+    displayDesc.Height      = RS::Config::Get()->Fetch<uint32>("Display/DefaultHeight", 1080);
+    displayDesc.Fullscreen  = RS::Config::Get()->Fetch<bool>("Display/Fullscreen", false);
+    displayDesc.VSync       = RS::Config::Get()->Fetch<bool>("Display/VSync", true);
     RS::Display::Get()->Init(displayDesc);
     RS::Input::Get()->Init();
 
