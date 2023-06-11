7c0ac8c7420c2ce6f6c653fc90cd97cdc5d86f0b
Jonathan Åleskog <jonathan9609082000@gmail.com>
2023-06-11 02:12:57 +0200

    Began thinking about the renderer.

--- Projects/Sandbox/Src/Render/Renderer.cpp
+++ Projects/Sandbox/Src/Render/Renderer.cpp
@@ -18,3 +18,21 @@ void Renderer::PreRender()
 void Renderer::PostRender()
 {
 }
+
+void RS::Renderer::Submit(uint64 sortIndex, const Group& group)
+{
+	std::unique_lock lock(m_GroupDataMutex);
+	m_UnprocessedGroups.push_back(group);
+}
+
+void RS::Renderer::EndFrame()
+{
+	std::unique_lock lock(m_GroupDataMutex);
+	RS_ASSERT(m_Groups.empty(), "Should not be any groups left!");
+	//m_Groups = m_UnprocessedGroups;
+}
+
+void RS::Renderer::RenderThread()
+{
+	// C
+}
